259e61e4029ebc1e0e980df1b2a849510de01678
anhnpa67khmt-hub <anh.npa.67khmt@ntu.edu.vn>
2025-11-17 10:36:11 +0700

    Create tong_tich_cua_hai_so_nguyen.py

---
+++ tong_tich_cua_hai_so_nguyen.py
@@ -0,0 +1,9 @@
+so_1 = int(input("mời nhập số nguyên thứ nhất: "))
+so_2 = int(input("mời nhập số nguyên thứ hai: "))
+tong= so_1 + so_2
+tich= so_1 * so_2
+print("kết quả tính toán:")
+print("số thứ nhất đã nhập:", so_1)
+print("số thứ nhất đã nhập:", so_2)
+print("tong của hai số là:", tong)
+print("tich của hai số là:", tich)
