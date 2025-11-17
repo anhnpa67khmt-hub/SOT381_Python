550c27d1775e7ca5423a466e31e7fb002255d1af
anhnpa67khmt-hub <anh.npa.67khmt@ntu.edu.vn>
2025-11-17 13:10:33 +0700

    Create tinh_tuoi.py

---
+++ tinh_tuoi.py
@@ -0,0 +1,7 @@
+NAM_HIEN_TAI = 2024
+nam_sinh = int(input("Mời nhập năm sinh của bạn: "))
+tuoi_hien_tai = NAM_HIEN_TAI - nam_sinh
+print("------------------------------")
+print("Năm sinh bạn đã nhập là:", nam_sinh)
+print("Năm hiện tại đang giả sử là:", NAM_HIEN_TAI)
+print("Tuổi hiện tại của bạn là:", tuoi_hien_tai, "tuổi.")
