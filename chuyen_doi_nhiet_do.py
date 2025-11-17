9a0aa2cd15137944c3d86ff30d760ea08d48380a
anhnpa67khmt-hub <anh.npa.67khmt@ntu.edu.vn>
2025-11-17 11:07:46 +0700

    Create chuyen_doi_nhiet_do.py

---
+++ chuyen_doi_nhiet_do.py
@@ -0,0 +1,5 @@
+so_1 = int(input(" mời nhập độ celsius: "))
+Fahrenheit = (so_1 * 9/5) +32
+print(" kết quả tính toán: ")
+print(" độ celsius đã nhập: ", so_1)
+print(" độ Fahrenhit là: ", Fahrenheit)
