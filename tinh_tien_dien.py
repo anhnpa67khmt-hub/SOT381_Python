f2369572068e9b46c3500179ccd03a636baa85af
anhnpa67khmt-hub <anh.npa.67khmt@ntu.edu.vn>
2025-11-17 11:40:59 +0700

    Create tinh_tien_dien.py

---
+++ tinh_tien_dien.py
@@ -0,0 +1,5 @@
+so_1 = int(input(" mời nhập số kWh điện tiêu thụ: "))
+tien_dien = so_1 * 2,000
+print(" kết quả tính toán: ")
+print(" số kWh điện tiêu thụ đã nhập: ", so_1)
+print(" số tiền điện là: ", tien_dien)
