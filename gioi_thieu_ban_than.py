5630af40caac592e4da865b0d2a0bcb2b7506501
anhnpa67khmt-hub <anh.npa.67khmt@ntu.edu.vn>
2025-11-17 13:54:27 +0700

    Create gioi_thieu_ban_than.py

---
+++ gioi_thieu_ban_than.py
@@ -0,0 +1,7 @@
+ho_ten = input("Mời nhập Họ tên của bạn: ")
+tuoi = input("Mời nhập Tuổi của bạn: ")
+que_quan = input("Mời nhập Quê quán của bạn: ")
+cau_gioi_thieu = f"Xin chào, tôi là {ho_ten}, {tuoi} tuổi, đến từ {que_quan}."
+print("---------------------------------------")
+print("Câu giới thiệu của bạn là:")
+print(cau_gioi_thieu)
