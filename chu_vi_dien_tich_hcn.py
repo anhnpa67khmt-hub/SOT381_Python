a57c70e1a2c0067ab8929549e838b4b4abffc241
anhnpa67khmt-hub <anh.npa.67khmt@ntu.edu.vn>
2025-11-17 10:55:45 +0700

    Create chu_vi_dien_tich_hcn.py

---
+++ chu_vi_dien_tich_hcn.py
@@ -0,0 +1,9 @@
+so_1 = int(input("mời nhập chiều dài: "))
+so_2 = int(input("mời nhập chiều rộng: "))
+chu_vi_hcn = (so_1 + so_2) * 2
+dien_tich_hcn = so_1 * so_2
+print(" kết quả tính toán: ")
+print(" chiều dài đã nhập: ", so_1)
+print(" chiều rộng đã nhập: ", so_2)
+print(" chu vi hcn là: ", chu_vi_hcn)
+print(" diện tích hcn là: ", dien_tich_hcn)
