63ef49fabd6a2fd1c3bf61bd5fdf5f72b5ee2ef0
anhnpa67khmt-hub <anh.npa.67khmt@ntu.edu.vn>
2025-12-02 20:18:52 +0700

    Create Ép kiểu dữ liệu.py

---
+++ Ép kiểu dữ liệu.py
@@ -0,0 +1,8 @@
+a = "25"
+b = 3
+c = int(a) + b
+d = str(b) + a
+print(f"Giá trị của c: {c}")
+print(f"Kiểu dữ liệu của c: {type(c)}")
+print(f"Giá trị của d: {d}")
+print(f"Kiểu dữ liệu của d: {type(d)}")
