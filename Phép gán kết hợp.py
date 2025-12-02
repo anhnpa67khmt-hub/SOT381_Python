dd23430828b185221eee408799f57bcc4bef6d07
anhnpa67khmt-hub <anh.npa.67khmt@ntu.edu.vn>
2025-12-02 20:18:27 +0700

    Create Phép gán kết hợp.py

---
+++ Phép gán kết hợp.py
@@ -0,0 +1,9 @@
+x = 10
+print(f"Giá trị ban đầu của x: {x}")
+x += 5
+print(f"Sau x += 5, x có giá trị: {x}")
+x *= 2
+print(f"Sau x *= 2, x có giá trị: {x}")
+x -= 8
+print(f"Sau x -= 8, x có giá trị: {x}")
+print(f"\nGiá trị cuối cùng của x là: {x}")
