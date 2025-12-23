b71a85639349009b4378b44521b718f4b796ad96
anhnpa67khmt-hub <anh.npa.67khmt@ntu.edu.vn>
2025-12-23 16:11:03 +0700

    Delete TH2212_Bai1.py
    Xóa file cũ để dọn dẹp kho

--- TH2212_Bai1.py
+++
@@ -1,6 +0,0 @@
-w = float(input("Nhập w: "))
-h = float(input("Nhập h: "))
-chu_vi = (w + h) * 2
-dien_tich = w * h
-print(f"Chu vi: {chu_vi:.2f}")
-print(f"Diện tích: {dien_tich:.2f}")                
