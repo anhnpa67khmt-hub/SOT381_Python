71881cddfd2d1ad61fa77ff8797a4cf8ab529bcf
anhnpa67khmt-hub <anh.npa.67khmt@ntu.edu.vn>
2025-12-27 08:50:51 +0700

    Update Tìm số Armstrong.py

--- Tìm số Armstrong.py
+++ Tìm số Armstrong.py
@@ -1,13 +1,8 @@
-try:
-    so_nguyen = input("Nhập một số để kiểm tra số Armstrong: ")
-    n_chu_so = len(so_nguyen)
-    tong_luy_thua = 0
-    for chu_so_ky_tu in so_nguyen:
-        chu_so = int(chu_so_ky_tu)
-        tong_luy_thua += chu_so ** n_chu_so
-    if tong_luy_thua == int(so_nguyen):
-        print(f"Số {so_nguyen} là số Armstrong.")
-    else:
-        print(f"Số {so_nguyen} không phải là số Armstrong.")
-except ValueError:
-    print("Đầu vào không hợp lệ.")
+so = input("Nhập số: ")
+n = len(so)
+tong = sum(int(chu_so)**n for chu_so in so)
+
+if tong == int(so):
+    print(f"{so} là số Armstrong")
+else:
+    print(f"{so} không phải số Armstrong")
